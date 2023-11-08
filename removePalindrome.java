38153177a2e3ce84372bb2c6717f0270af84884c
Harish12121 <harishmsece@gmail.com>
2023-11-08 15:00:18 +0530

    removePalindrome.java added

---
+++ removePalindrome.java
@@ -0,0 +1,35 @@
+/**
+ * removePalindrome
+ */
+public class removePalindrome {
+
+    public static void main(String[] args) {
+        String str = "I saw the racecar yesterday";
+        String[] arr = str.split(" "); 
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0;i<arr.length;i++){
+            if(!isPalindrome(arr[i])){
+                ans.append(arr[i]).append(" ");
+            }
+        }
+        System.out.println(ans.toString());
+
+    }
+
+    private static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length()-1;
+        while (i<j) {
+            if(str.charAt(i)==str.charAt(j)){
+                continue;
+            }else{
+                return false;
+            }  
+            i++;
+            j--;      
+        }
+        return true;
+    }
+    
+}
