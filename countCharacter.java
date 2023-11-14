6e7de6d189c83b189c5096d954410e3d6959263f
Harish12121 <harishmsece@gmail.com>
2023-11-14 11:59:50 +0530

    Alignment changed

--- countCharacter.java
+++ countCharacter.java
@@ -1,32 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
-
 public class countCharacter {
     public static String countCharacterFrequency(String input) {
         if (input == null || input.isEmpty()) {
             return "";
         }
-
         Map<Character, Integer> charFrequency = new HashMap<>();
-
         for (char c : input.toCharArray()) {
             charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
         }
-
         StringBuilder output = new StringBuilder();
-
         for (Map.Entry<Character, Integer> entry : charFrequency.entrySet()) {
             output.append(entry.getValue()).append(entry.getKey());
         }
-
         return output.toString();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String input = sc.nextLine();                       //aaaffhhxx
         String result = countCharacterFrequency(input);
         System.out.println("Input: " + input);              //Input: aaaffhhxx
